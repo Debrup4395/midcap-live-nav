925714fc641ca34bde1ce014ed0ed64c625f5af1
SOUMOSHREE MALLIK <kingdebrup@gmail.com>
2026-05-13 22:06:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -632,43 +632,6 @@ if len(fund_performance) > 0:
 # =====================================================
 
 st.markdown("---")
-# =====================================================
-# BEST & WORST FUND
-# =====================================================
-
-st.markdown("---")
-
-if len(fund_performance) > 0:
-
-    performance_df = pd.DataFrame(fund_performance)
-
-    best_fund = performance_df.loc[
-        performance_df["Return"].idxmax()
-    ]
-
-    worst_fund = performance_df.loc[
-        performance_df["Return"].idxmin()
-    ]
-
-    st.subheader("🏆 Fund Performance Today")
-
-    col1, col2 = st.columns(2)
-
-    col1.metric(
-        "Best Fund of the Day",
-        best_fund["Fund"],
-        f"{best_fund['Return']:.2f}%"
-    )
-
-    col2.metric(
-        "Worst Fund of the Day",
-        worst_fund["Fund"],
-        f"{worst_fund['Return']:.2f}%"
-    )
-st.caption(
-    "Live NAV estimation based on weighted portfolio stock movement."
-)
-st.markdown("---")
 
 st.caption(
     "Live NAV estimation based on weighted portfolio stock movement."
